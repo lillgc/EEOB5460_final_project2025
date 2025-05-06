e6264b095b27b2a55ad1d8d9adb2aa79f114296a
lillgc <lilliangchiang@gmail.com>
2025-05-05 20:25:59 -0500

    inspect_genomes.sh updated (works)

--- inspect_genomes.sh
+++ inspect_genomes.sh
@@ -4,13 +4,29 @@
 set -e
 
 # -------------------
+# 1. Micromamba Set Up
+# -------------------
 
-# quality control w/ fastp
-#module load fastp 
-#micromamba install -c bioconda fastp
-#brew install fastp
+micromamba create -y -n mngs_env fastp seqtk megahit bowtie2 samtools blast diamond metaphlan2 mafft phyml bcftools -c bioconda -c conda-forge
+
+eval "$(micromamba shell hook --shell bash)"
+micromamba activate mngs_env
+
+for tool in fastp megahit blastn diamond samtools bcftools mafft phyml; do
+  if ! command -v $tool &> /dev/null; then
+    echo "[ERROR] Tool '$tool' missing. Check environment setup."
+    exit 1
+  fi
+done
+
+
+# -------------------
+# 2. Quality Control
+# -------------------
+#module load fastp
 
 # output cleaned fastq files (trimmed adapters) and html/json qc reports
+
 ## wuhan 1
 fastp -i patient_raw_fastq/SRR10903402_1.fastq -I patient_raw_fastq/SRR10903402_2.fastq \
   -o working_files/wuhan1_clean_1.fastq -O working_files/wuhan1_clean_2.fastq \
@@ -22,10 +38,13 @@ fastp -i patient_raw_fastq/SRR10903401_1.fastq -I patient_raw_fastq/SRR10903401_
   --detect_adapter_for_pe --thread 8 --html output_files/reports/wuhan2_fastp.html --json output_files/reports/wuhan2_fastp.json
 
 
-# data prep for blastn (fastq --> fasta)
+# -------------------
+# 3. Blastn prep
+# -------------------
+
 #module load seqtk
-#micromamba install -c bioconda seqtk # may need to alter if using other conda type (micromamba works in ISU HPC)
-#brew install seqtk
+
+# data prep for blastn (fastq --> fasta)
 
 ## wuhan 1
 cat working_files/wuhan1_clean_1.fastq working_files/wuhan1_clean_2.fastq > working_files/wuhan1_merged.fastq
@@ -36,8 +55,6 @@ cat working_files/wuhan2_clean_1.fastq working_files/wuhan2_clean_2.fastq > work
 seqtk seq -a working_files/wuhan2_merged.fastq > working_files/wuhan2_merged.fasta
 
 #module load ncbi-rmblastn/2.14.0-py310-vqnew3z
-# use "module spider blastn" to search for available versions
-#brew install blast
 
 
 # loading blast db
@@ -65,5 +82,17 @@ blastn -query working_files/wuhan2_merged.fasta \
        -evalue 1e-5 \
        -outfmt 6 \
        -num_threads 8
+
+
+# -------------------------
+# 4. Assembly using Megabit
+# -------------------------
+
+ echo "[2] MEGAHIT"
+   
+megahit -1 wuhan1_merged.fastq -2 wuhan2_merged.fastq -o working_files/megahit_out.fa --min-contig-len 500
+
+
+
 
 
